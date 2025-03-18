0a4c619693fbc91e12fbb70ab9aa857a04e328a7
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-18 13:22:52 +0100

    kot14

--- cypress/e2e/ui/isolation/register.ui.isolated.cy.ts
+++ cypress/e2e/ui/isolation/register.ui.isolated.cy.ts
@@ -6,13 +6,13 @@ import { registerPage } from "../../../pages/registerPage"
 
 describe('Register tests in isolation', () => {
     beforeEach(() => {
-      cy.visit('/register')
+        cy.visit('/register')
     })
-  
+
     it('should successfully register new user', () => {
         // given
         const user = getRandomUser()
-        
+
         // when
         registerPage.attemptRegister(user)
 
@@ -20,5 +20,17 @@ describe('Register tests in isolation', () => {
         toast.verifySuccess('Registration successful! You can now log in.')
         cy.url().should('contain', '/login')
     })
-  })
-  
+
+    it('should fail to register if user already exists', () => {
+        // given
+        const user = getRandomUser()
+        cy.register(user)
+
+        // when
+        registerPage.attemptRegister(user)
+
+        // then
+        toast.verifyError('Username already exists')
+        cy.url().should('contain', '/register')
+    })
+})

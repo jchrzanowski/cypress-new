2cc2162797a36008f670b9fa9fab9581fda8c0ec
10002683 <Jakub.Chrzanowski@ergo.com>
2025-03-18 13:13:33 +0100

    kot12

---
+++ cypress/e2e/ui/register.ui.isolated.cy.ts
@@ -0,0 +1,21 @@
+import { getRandomUser } from "../../generators/userGenerator"
+import { toast } from "../../pages/components/toast"
+import { registerPage } from "../../pages/registerPage"
+
+describe('Register tests in isolation', () => {
+    beforeEach(() => {
+        cy.visit('/register')
+    })
+
+    it('should successfully register new user', () => {
+        // given
+        const user = getRandomUser()
+
+        // when
+        registerPage.attemptRegister(user)
+
+        // then
+        toast.verifySuccess('Registration successful! You can now log in.')
+        cy.url().should('contain', '/login')
+    })
+})

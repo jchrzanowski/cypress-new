ee0b9345fde107fb0fe3b9724c64c84ddef5a846
10002683 <Jakub.Chrzanowski@ergo.com>
2025-03-18 13:30:22 +0100

    kot13

--- cypress/e2e/ui/register.ui.isolated.cy.ts
+++ cypress/e2e/ui/register.ui.isolated.cy.ts
@@ -18,4 +18,16 @@ describe('Register tests in isolation', () => {
         toast.verifySuccess('Registration successful! You can now log in.')
         cy.url().should('contain', '/login')
     })
+    it('should fail to register if user already exists', () => {
+        // given
+        const user = getRandomUser()
+        cy.register(user)
+
+        // when
+        registerPage.attemptRegister(user)
+
+        // then
+        toast.verifyError('Username already exists')
+        cy.url().should('contain', '/register')
+    })
 })
--- cypress/pages/components/toast.ts
+++ cypress/pages/components/toast.ts
@@ -1,7 +1,12 @@
 export const toast = {
 
     verifySuccess: (message: string) => {
+        cy.get('._title_gmcqp_44').should('have.text', 'Success')
+        cy.get('._description_gmcqp_50').should('have.text', message)
+    },
+
+    verifyError: (message: string) => {
+        cy.get('._title_gmcqp_44').should('have.text', 'Error')
         cy.get('._description_gmcqp_50').should('have.text', message)
     }
-
 }
